0945753612c4c1d0be72c13c364445692f4b7785
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-14 19:33:30 -0400

    implements alternative solution to injecting fetched CSV data into the Table component

--- components/Table.js
+++ components/Table.js
@@ -1,43 +1,92 @@
 import Row from "./Row";
 import React from "react";
 
-export default class Table extends React.Component {
-  constructor(props) {
-    super(props);
+// export default class Table extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      data: {},
-    };
-  }
+//     this.state = {
+//       data: {},
+//     };
+//   }
 
-  handleChangedCell = ({ x, y }, value) => {
-    const modifiedData = Object.assign({}, this.state.data);
+//   handleChangedCell = ({ x, y }, value) => {
+//     const modifiedData = Object.assign({}, this.state.data);
 
-    if (!modifiedData[y]) modifiedData[y] = {};
-    modifedData[y][x] = value;
-    this.setState({ data: modifiedData });
-  };
+//     if (!modifiedData[y]) modifiedData[y] = {};
+//     modifedData[y][x] = value;
+//     this.setState({ data: modifiedData });
+//   };
 
-  updateCells = () => {
-    this.forceUpdate();
-  };
+//   updateCells = () => {
+//     this.forceUpdate();
+//   };
 
-  render() {
-    const rows = [];
+//   render() {
+//     const rows = [];
 
-    for (let y = 0; y < this.props.y + 1; y += 1) {
-      const rowData = this.state.data[y] || {};
-      rows.push(
-        <Row
-          handleChangedCell={this.handleChangedCell}
-          updateCells={this.updateCells}
-          key={y}
-          y={y}
-          x={this.props.x + 1}
-          rowData={rowData}
-        />
-      );
-    }
-    return <div>{rows}</div>;
-  }
-}
+//     for (let y = 0; y < this.props.y + 1; y += 1) {
+//       const rowData = this.state.data[y] || {};
+//       rows.push(
+//         <Row
+//           handleChangedCell={this.handleChangedCell}
+//           updateCells={this.updateCells}
+//           key={y}
+//           y={y}
+//           x={this.props.x + 1}
+//           rowData={rowData}
+//         />
+//       );
+//     }
+//     return <div>{rows}</div>;
+//   }
+// }
+
+const Table = ({ apiResponse }) => {
+  // console.log(apiResponse);
+  // for (let i in apiResponse) {
+  //   // console.log(apiResponse[i]);
+  //   for (let j in apiResponse) {
+  //     // console.log(apiResponse[j])
+  //   }
+  // }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Date/Time</th>
+          <th>Symbol</th>
+          <th>Open</th>
+          <th>High</th>
+          <th>Low</th>
+          <th>Close</th>
+          <th>Volume</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* {apiResponse.length > 0 ? (
+          apiResponse.rows.forEach((row, index) => {
+            return (
+              <tr key={index}>
+                <td>{row.dateTime}</td>
+                <td>{row.symbol}</td>
+                <td>{row.open}</td>
+                <td>{row.high}</td>
+                <td>{row.low}</td>
+                <td>{row.close}</td>
+                <td>{row.volume}</td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan="5">Loading...</td>
+          </tr>
+        )} */}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,39 @@ import styles from "../styles/Home.module.css";
 import Table from "../components/Table";
 import React, { Component } from "react";
 
-export default class Home extends Component {
+const expressUrl = "http://localhost:5000/csv";
+
+class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiResponse: "",
-      // we want to send updated rows and columns
-      data: "",
+      apiResponse: [],
+      // apiResponse: "",
+
     };
   }
 
-  callAPI() {
-    fetch("http://localhost:9000/", { withCredentials: true })
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch((err) => err);
+  componentDidMount() {
+    fetch("http://localhost:5000/csv")
+      // .then((res) => res.text())
+      // .then((res) => this.setState({ apiResponse: res }))
+      // .catch((err) => err);
+      .then((res) => res.json())
+      // .then((json) => json.apiResponse)
+      .then((res) => this.setState({ apiResponse: res }));
   }
 
-  postData() {}
+  // componentWillMount() {
+  //   this.callAPI();
+  // }
 
-  componentWillMount() {
-    this.callAPI();
-  }
   render() {
     return (
       <div className={styles.container}>
-        <Table x={4} y={4} />
-
-        <p className="App-intro">{this.state.apiResponse}</p>
+        <Table apiResponse={this.state.apiResponse} />
       </div>
     );
   }
 }
+
+export default Home;
